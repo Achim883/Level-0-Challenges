afb716033c98b780844c54855f399c551820decb
Annette Tchouawou <annettetchouawou@gmail.com>
2021-08-18 03:27:00 +0100

    corrections to task 5

--- task05.js
+++ task05.js
@@ -1,8 +1,11 @@
 function getAreaTriangle(side1,side2,side3){
+    var area = 0;
     if ( side1 > 0 & side2 > 0 & side3 > 0) {
-        var area = 0.5 * (side1 + side2 + side3);
+        var semiperimeter = 0.5 * (side1 + side2 + side3);
+        area = Math.sqrt( (semiperimeter * (semiperimeter-side1) * (semiperimeter - side2) * (semiperimeter-side3)));
         return area;
     } else {
         return "Please enter a positve number."
     }
 }
+getAreaTriangle(3,4,5);
