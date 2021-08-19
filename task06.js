549ee6806252677a290345ef3285c38143330029
Achim883 <86787228+Achim883@users.noreply.github.com>
2021-08-19 05:31:18 +0100

    Update task06.js
    Helle. As required, I removed the first function.

--- task06.js
+++ task06.js
@@ -1,15 +1,3 @@
-function findMaximum (num1,num2,num3){
-    var max=0;
-    if (num1 >= num2 ){
-        max = num1;
-    } else {
-        max = num2;
-    }if (max >= num3){
-        return max;
-    } else{
-        return num3;
-    }
-}
 function findMaximum(...num){
     var max = 0;
     for ( i=0; i <= num.length; i ++) {
